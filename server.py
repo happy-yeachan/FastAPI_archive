498b28477051e06e9a2bf0691e18cdf6a08cad7c
yeachan <dus001228@naver.com>
2024-07-03 11:16:56 +0900

    :pencil2: Test 파일 이름 변경

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("http_ex:app", host="localhost", port= 8000, reload=False)
+    uvicorn.run("main:app", host="localhost", port= 8000, reload=False)
